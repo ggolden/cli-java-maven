4ae05cf541c67ec1fca91ae2ea58984fe4f913c8
Glenn R. Golden <ggolden@etudes.org>
2017-05-13 11:10:17 -0400

    initial commit

---
+++ cli/src/test/java/org/ggolden/cli/cli/CliTest.java
@@ -0,0 +1,32 @@
+/**********************************************************************************
+ *
+ * Copyright 2017 Glenn R. Golden 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.ggolden.cli.cli;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class CliTest
+{
+	@Test
+	public void test_1()
+	{
+		System.out.println("CLI Test");
+		Assertions.assertThat(true).isTrue();
+	}
+}
